dc166eda33923dec5becd600d8da2faf10d116ca
v <cryi@tutanota.com>
2025-03-09 16:33:07 +0100

    version bump

--- constants/common.go
+++ constants/common.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	VERSION = "0.16.5-beta"
+	VERSION = "0.16.7-beta"
 
 	DefaultBBDirectory       string = "/bake-buddy"
 	DefaultRemoteBBDirectory string = DefaultBBDirectory
