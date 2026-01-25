f1d85dcb9125c0847b854e1f40c73d95b95ff1b5
ciripel <nedelcu.alexandru@yahoo.com>
2026-01-25 12:22:23 +0200

    log util

--- util/log.go
+++ util/log.go
@@ -1,47 +1,46 @@
 package util
 
 import (
+	"log/slog"
 	"os"
-
-	log "github.com/sirupsen/logrus"
 )
 
 func AssertSB(check bool, msg string) {
 	if !check {
-		log.Error(msg)
+		slog.Error(msg)
 	}
 }
 
 func AssertB(check bool, msg string) {
 	if !check {
-		log.Error(msg)
+		slog.Error(msg)
 		os.Exit(-1)
 	}
 }
 
 func AssertBE(check bool, msg string, exitCode int) {
 	if !check {
-		log.Error(msg)
+		slog.Error(msg)
 		os.Exit(exitCode)
 	}
 }
 
 func AssertSE(err error, msg string) {
 	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error(msg)
+		slog.Error(msg, "error", err)
 	}
 }
 
 func AssertE(err error, msg string) {
 	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error(msg)
+		slog.Error(msg, "error", err)
 		os.Exit(-1)
 	}
 }
 
 func AssertEE(err error, msg string, exitCode int) {
 	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error(msg)
+		slog.Error(msg, "error", err)
 		os.Exit(exitCode)
 	}
 }
